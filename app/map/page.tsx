7ca01b0921c25448c5b435a07623c47543854660
Eliseo Alcaraz <elipalcaraz@gmail.com>
2025-10-03 00:59:47 +0800

    fix: ts error

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -23,7 +23,7 @@ import { dummyReports } from "@/data/dummy-reports";
 import ReactDOM from "react-dom/client";
 import { ReportBubble, type ReportBubbleRef } from "@/components/report-bubble";
 
-//@ts-ignore
+// @ts-expect-error
 import "mapbox-gl/dist/mapbox-gl.css";
 
 export default function MapPage() {
