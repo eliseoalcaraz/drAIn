6e476e8cbce2eae5cd0e5d44a7ea345bdb14bdc0
Kint <kborbano@zrc.pshs.edu.ph>
2025-10-13 11:49:15 +0800

    fix: pnpm gaurd rail

--- components/nav-main.tsx
+++ components/nav-main.tsx
@@ -20,19 +20,18 @@ import {
   SidebarMenuSubItem,
 } from "@/components/ui/sidebar";
 
-type HeroIcon = React.ComponentType<React.SVGProps<SVGSVGElement>>;
 export function NavMain({
   items,
 }: {
   items: {
     title: string;
     url?: string;
-    icon?: HeroIcon;
+    icon?: React.ComponentType<any>;
     isActive?: boolean;
     items?: {
       title: string;
       url: string;
-      icon?: HeroIcon;
+      icon?: React.ComponentType<any>;
     }[];
   }[];
 }) {
