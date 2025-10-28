df1d5688c3cd1cbe747d6a2b9d37f38b06b4b0d9
Kint <kborbano@zrc.pshs.edu.ph>
2025-10-28 21:11:57 +0800

    fix: lint error

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -13,7 +13,6 @@ import {
 
 import { NavMain } from "@/components/nav-main";
 import { NavUser } from "@/components/nav-user";
-import { TeamSwitcher } from "@/components/team-switcher";
 import {
   Sidebar,
   SidebarContent,
